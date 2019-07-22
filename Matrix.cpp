f86dfa225d12adf14716502392baecdd2e792a71
tomaz1502 <tomgm1502@gmail.com>
2019-07-22 19:53:05 -0300

    Matrix fixed

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,41 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const long long mod = 1e9 + 7;
 
 template <typename T>
 class Matrix{
 	vector< vector<T> > M;
 
 public:
-	Matrix(long long rows, long long columns, bool identity = false){
+	Matrix(int rows, int columns, bool identity = false){
 		M = vector< vector<T> >(rows, vector<T>(columns, 0));
 		assert(rows == columns or identity == false);
-		if(identity) for(long long i = 0; i<rows; ++i) this->update(i,i,1);
+		if(identity) for(int i = 0; i<rows; ++i) this->update(i,i,1);
 
 	}
 
-	Matrix(vector< vector<T> > V){
+	Matrix(const vector< vector<T> > &V){
 		M = V;
 	}
 
-	long long size() const{
+	int size() const{
 		return this->M.size();
 	}
 
-	T get(long long i, long long j) const{
+	T get(int i, int j) const{
 		return M[i][j];
 	}
 
-	vector<T> get(long long i) const{
+	vector<T> get(int i) const{
 		return M[i];
 	}
 
-	vector<T>& operator[](long long i){
+	vector<T>& operator[](int i){
 		return M[i];
 	}
 
-	void update(long long i, long long j, T value){
+	void update(int i, int j, T value){
 		M[i][j] = value;
 	}
 
@@ -43,10 +42,10 @@ public:
 		Matrix<T> ret = Matrix(M.size(), M[0].size());
 		assert(M.size() == M_.size());
 
-		for(long long i = 0; i<M.size(); ++i){
+		for(int i = 0; i<M.size(); ++i){
 			assert(M[i].size() == M_.get(i).size());
 
-			for(long long j = 0; j<M[i].size(); ++j){
+			for(int j = 0; j<M[i].size(); ++j){
 				ret.update(i,j, M[i][j] + M_.get(i,j));
 			}
 		}
@@ -58,10 +57,10 @@ public:
 		assert(M[0].size() == M_.size());
 		Matrix<T> ret = Matrix(M.size(), M_.get(0).size());
 
-		for(long long i = 0; i<M.size(); ++i){
-			for(long long j = 0; j<M_.get(0).size(); ++j){
-				long long c = 0;
-				for(long long k = 0; k<M_.size(); ++k) c = (c + M[i][k] * M_.get(k,j))%mod;
+		for(int i = 0; i<M.size(); ++i){
+			for(int j = 0; j<M_.get(0).size(); ++j){
+				int c = 0;
+				for(int k = 0; k<M_.size(); ++k) c = (c + M[i][k] * M_.get(k,j));
 				ret.update(i,j,c);
 			}
 		}
@@ -69,7 +68,7 @@ public:
 		return ret;
 	}
 
-	Matrix<T> operator^(long long exp) const{
+	Matrix<T> operator^(int exp) const{
 		assert(M.size() == M[0].size()); //M is a square matrix
 		Matrix<T> ret = Matrix<T>(M.size(), M[0].size(), true);
 		Matrix<T> aux = Matrix<T>(M);
@@ -87,8 +86,8 @@ public:
 
 template<typename T>
 ostream& operator<<(ostream &out, Matrix<T> &M){
-	for(long long i = 0; i<M.size(); ++i, out << '\n'){
-		for(long long j = 0; j<M[i].size(); ++j) out << M[i][j] << ' ';
+	for(int i = 0; i<M.size(); ++i, out << '\n'){
+		for(int j = 0; j<M[i].size(); ++j) out << M[i][j] << ' ';
 	}
 	return out;
 }
