df288a9dc9343350430be63b2af93633d8e820ad
tomaz1502 <tomgm1502@gmail.com>
2019-07-22 19:43:49 -0300

    MinQueue looks better :)

--- MinQueue.cpp
+++ MinQueue.cpp
@@ -1,41 +1,93 @@
 #include <cassert>
 #include <stack>
 #include <iostream>
-using namespace std;
 
-using minStack = stack< pair<long long, long long> >; //second: min element below at
+template < typename T > // T must have operator< defined
+class minQueue{
+    
+    using minStack = std::stack< std::pair < T , T > >; // second coordinate: smallest element from current to bottom
 
-struct minQueue{
+private:
+
     minStack in, out;
 
-    #define INF (long long)1e16
+public:
 
-    minQueue(){ in.push({INF, INF}); out.push({INF, INF}); }
+    minQueue() { }
 
-    void push(long long x){
-        long long m = in.top().second;
-        in.push({x, min(x, m)});
+    void push(T x) {
+
+        if(in.empty()) {
+            in.emplace(x, x);
+        }
+
+        else{
+            T m = in.top().second; // previous minimum
+            in.emplace(x, std::min(x, m));
+        }
+
     }
 
-    long long pop(){
-        if(out.top().first == INF){
-            while(in.top().first != INF){
-                pair<long long, long long> p = in.top(); in.pop();
-                long long m = out.top().second;
-                out.push({p.first, min(p.first, m)});
+    T pop(){
+        if(out.empty()){
+
+            while(!in.empty()){ // transfers everything to out stack (inverting their order)
+
+                std::pair<T, T> p = in.top(); in.pop();
+                    
+                if(out.empty()) {
+                    out.emplace(p.first, p.first);
+                }
+                
+                else{
+                    T m = out.top().second; 
+                    out.emplace(p.first, std::min(p.first, m));
+                }
             }
+
         }
-        long long x = out.top().first; out.pop();
-        assert(x != INF);
+
+        if(out.empty()) {
+        
+            std::cout << "Error! Queue is empty.\n";
+            assert(false);
+        
+        }
+        
+        T x = out.top().first; out.pop();
+     
         return x;
     }
 
-    long long getMin(){ return min(in.top().second, out.top().second); }
+    T getMin() {
 
+        if(in.empty() and out.empty()) {
+            std::cout << "Can't get minimum: Queue is empty.\n";
+            assert(false);
+        }
+        
+        if(in.empty()) return out.top().second;
+        if(out.empty()) return in.top().second;
+
+        return std::min(in.top().second, out.top().second);
+    }
 
 };
 
 int main(){
+
+    minQueue< char > MQ;
+    
+    MQ.push('a');
+    MQ.push('x');
+    MQ.push('h');
+    
+    std::cout << MQ.getMin() << '\n';
+
+    std::cout << MQ.pop() << '\n';
+
+    std::cout << MQ.getMin() << '\n';
+
 
     return 0;
 }
