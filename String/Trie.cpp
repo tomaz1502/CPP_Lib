f55e8ddeb1eaeb75c242734d5c76b82bca8da812
tomaz1502 <tomgm1502@gmail.com>
2020-04-23 23:36:29 -0300

    Trie

--- String/Trie.cpp
+++ String/Trie.cpp
@@ -1,33 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Trie{
+struct Trie {
 	vector< vector<int> > T;
 	vector<bool> endOfWord;
-	int prox = 1;
+	int proxNode = 1;
 
-public:
-
-	Trie(int maxNodes, int alphabetSize){ //maxNodes is the maximum amount of letters allowed
+	Trie(int maxNodes, int alphabetSize) { //maxNodes is the maximum amount of letters allowed
 		T = vector< vector<int> >(maxNodes, vector<int>(alphabetSize, 0));
 		endOfWord = vector<bool>(maxNodes, false);
 	}
 
-	void insert(string s){
+	void insert(string s) {
 		int at = 1;
-		for(int i = 0; i<s.length(); ++i){
+		for(int i = 0; i < (int)s.length(); ++i) {
 			int let = s[i] - 'a'; //base letter of the alphabet
 			if(T[at][let] == 0){
-				T[at][let] = ++this->prox;				
+				T[at][let] = ++this->proxNode;				
 			}
 			at = T[at][let];
-			if(i == s.length() - 1) endOfWord[at] = true;
+			if(i == (int)s.length() - 1) endOfWord[at] = true;
 		}
 	}
 
-	bool search(string s){ // retorna falso se s é só o prefixo de alguém que tá na trie, e não uma palavra inteira
+	bool search(string s) { // retorna falso se s é só o prefixo de alguém que tá na trie, e não uma palavra inteira
 		int at = 1;
-		for(int i = 0; i<s.length(); ++i){
+		for(int i = 0; i < (int)s.length(); ++i) {
 			int let = s[i] - 'a';
 			if(T[at][let] == 0) return false;
 			at = T[at][let];
