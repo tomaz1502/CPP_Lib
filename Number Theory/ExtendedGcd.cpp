554d0013b1e71f3215acc759701f1e933d799b94
tomaz1502 <tomgm1502@gmail.com>
2020-04-23 21:37:00 -0300

    Folderization

---
+++ Number Theory/ExtendedGcd.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int extGcd(int a, int b, int &x, int &y) {
+    //TODO
+}
+
+int main() {
+
+
+    return 0;
+}
---
+++ Number Theory/Phi.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int Phi(int n) {
+    //TODO
+}
+
+int main() {
+
+
+    return 0;
+}
