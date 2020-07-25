9389763cafd50ffd895af5c8c303c33ec6688c80
tomaz1502 <tomgm1502@gmail.com>
2020-07-24 21:45:02 -0300

    std in lis

--- Classical/LIS.cpp
+++ Classical/LIS.cpp
@@ -1,12 +1,11 @@
 #include <vector>
 #include <cassert>
-using namespace std;
 
-int LIS(const vector< int > &a) { // Longest non-decreasing: troca lower por upper
+int LIS(const std::vector< int > &a) { // Longest non-decreasing: troca lower por upper
     int n = a.size(), lim = 1e8;
-    vector<int> d(n+1, lim);
+    std::vector<int> d(n+1, lim);
     for (int j = 0; j < n; j++) {
-        *lower_bound(d.begin(), d.end(), a[j]) = a[j];
+        *std::lower_bound(d.begin(), d.end(), a[j]) = a[j];
     }
     for(int j = 0; j<=n; j++) {
         if(d[j] == lim) return j;
