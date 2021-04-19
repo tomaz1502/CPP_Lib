e8740ba03b7e333036c4a6fd481254aa70657720
Tomaz Gomes <tomazgomes@pop-os.localdomain>
2021-04-19 15:45:00 -0300

    extended gcd

--- Number Theory/ExtendedGcd.cpp
+++ Number Theory/ExtendedGcd.cpp
@@ -1,7 +1,16 @@
 using namespace std;
 
 int extGcd(int a, int b, int &x, int &y) {
-    //TODO
+    if (a == 0) {
+        x = 0;
+        y = 1;
+        return b;
+    }
+    int _x, _y;
+    int d = extGcd(b % a, a, _x, _y);
+    x = _y - (b / a) * _x;
+    y = _x;
+    return d;
 }
 
 int main() {
