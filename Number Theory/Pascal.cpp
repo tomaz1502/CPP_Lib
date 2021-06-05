15b62465dca29f05182cbdb020f7346a6ffb24de
Tomaz Gomes <tomazgomes@pop-os.localdomain>
2021-06-05 14:09:51 -0300

    Pascal styling

--- Number Theory/Pascal.cpp
+++ Number Theory/Pascal.cpp
@@ -4,7 +4,7 @@ using ll = long long;
 
 const int MOD = 1e9 + 7;
 
-vector<vector<int>> bin_coefs(int L){
+vector<vector<int>> bin_coefs(int L) {
     vector<vector<int>> bin(L, vector<int>(L));
 	bin[0][0] = 1;
 	for (int i = 1; i < L; i++) {
